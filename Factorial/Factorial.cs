f46309a27dcf29ca5b53c5b1c22ffeebf8ee7e0a
hanslope12 <91108047+hanslope12@users.noreply.github.com>
2022-03-17 18:19:47 +0800

    Add files via upload

---
+++ Factorial/Factorial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Factorial
+    {
+        static void Main(string[] args)
+        {
+            int fact = 1;
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(fact);
+                fact *= i;
+            }
+            Console.WriteLine(fact);
+        }
+    }
+}
